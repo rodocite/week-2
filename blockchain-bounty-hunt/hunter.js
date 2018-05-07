a7190dd1812c8c040ef9917bf32d37d50dd15b00
Rodolfo Yabut <rodolfoyabut@gmail.com>
2018-05-07 11:53:24 -0700

    Refactor hunter

--- blockchain-bounty-hunt/hunter.js
+++ blockchain-bounty-hunt/hunter.js
@@ -5,19 +5,24 @@ const _ = require('lodash')
 const blockhashes = JSON.parse(fs.readFileSync('./blockhashes.json').toString())
 let index = 0
 
-setInterval(() => {
-  console.log('Inspecting ', blockhashes[index])
+const interval = setInterval(() => {
+  const blockhash = blockhashes.pop()
+  console.log('Inspecting ', blockhash)
+  fs.writeFileSync('./new.json', JSON.stringify(blockhashes))
 
-  request(`https://blockchain.info/rawblock/${blockhashes[index]}?format=json`, (req, res, body) => {
+  request(`https://blockchain.info/rawblock/${blockhash}?format=json`, (req, res, body) => {
     const transactions = JSON.parse(body).tx
     const pattern = '04dfa3'
-    const match = _.find(transactions, ({ hash }) => pattern === hash.slice(-6))
+    const match = _.find(transactions, ({ hash }) => {
+      console.log('---  transaction hash:' + hash)
+      return pattern === hash.slice(-6)
+    })
 
     if (match) {
       console.log('match', match)
-      fs.writeFileSync('./matchedtxhash', match)
+      fs.writeFileSync('./matchedtxhash.json', JSON.stringify(match))
+      clearInterval(interval)
+      return
     }
   })
-
-  index++
-}, 10000)
+}, 60000)
